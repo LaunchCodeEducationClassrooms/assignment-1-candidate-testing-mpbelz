6a58925e6d7949fb364c45334877d62eeb4bccb6
mpbelz <mpbelz@gmail.com>
2021-02-20 06:38:28 +0000

    Assignment completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -33,30 +33,42 @@ console.log(">>> Status: FAILED <<<");
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
-
+candidateName=input.question("Enter your name: ");
 }
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-
-
+for(i=0;i<questions.length;i++) {
+candidateAnswers[i]=input.question(questions[i]);
+}
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-
-
-  let grade;
-  
-
-  return grade;
+let totalCorrectAnswer = 0;
+for(let i = 0; i < questions.length; i++){
+  if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+     totalCorrectAnswer++;   
+}
+   console.log(`${questions[i]}`);
+   console.log(`your answer : ${candidateAnswers[i]}`);
+   console.log(`correct answer : ${correctAnswers[i]}`);
+}
+let grade=((totalCorrectAnswer/questions.length)*100) ;
+  if (grade >= 80) {
+    console.log(">>> Status: PASSED <<<");
+  } else {
+    console.log(">>> Status: FAILED <<<");
+  }
+  console.log(`>>> Overall Grade: ${grade} % (${totalCorrectAnswer} of ${questions.length} responses correct) <<<`); 
+ return grade;
 }
 
 function runProgram() {
   askForName();
   // TODO 1.1c: Ask for candidate's name //
-  
+  console.log(`Hello: ${candidateName}!`);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
 }
